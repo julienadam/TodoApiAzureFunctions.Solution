f646914e28e1bb56ec29a6ddf6bd816b412c8b2f
Julien Adam <julien.adam@arcodia.fr>
2023-05-14 14:32:33 +0200

    Initial commit with first test passing

---
+++ GetHome.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace TodoApiAzureFunctions
+{
+    public static class GetHome
+    {
+        [FunctionName("GetHome")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todos")] HttpRequest req, 
+            ILogger log)
+        {
+            log.LogInformation("Get on main page");
+            return new OkObjectResult("Foo");
+        }
+    }
+}
